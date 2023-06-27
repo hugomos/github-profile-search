36a6ac9b03feb5c3fdba3890c2f15efaa5d78b7c
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2023-06-27 16:49:16 -0300

    feat: construindo funcionalidades de busca e estilizando o layouy. @hugomos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,125 @@
-import Image from 'next/image'
+'use client'
+import { FormEvent, useState } from 'react'
+import {
+  Archive,
+  GithubLogo,
+  Users,
+  WarningCircle,
+  X,
+  Share,
+} from '@phosphor-icons/react'
+
+/* eslint-disable @next/next/no-img-element */
+
+export interface User {
+  login: string
+  avatar_url: string
+  public_repos: number
+  followers: number
+  bio: string
+}
 
 export default function Home() {
+  const [search, setSearch] = useState('')
+  const [user, setUser] = useState<User | null>(null)
+
+  const [showError, setShowError] = useState(false)
+
+  const getUserData = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setShowError(false)
+
+    const res = await fetch(`https://api.github.com/users/${search}`)
+    if (!res.ok) {
+      setShowError(true)
+      return
+    }
+
+    const data = await res.json()
+    setUser(data)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="h-screen w-full bg-zinc-950">
+      <div className="flex h-full flex-col items-center justify-center gap-4">
+        {user ? (
+          <div className="flex w-80 flex-col items-center justify-center gap-4 rounded bg-zinc-900 p-4">
+            <div className="flex w-full items-center justify-between">
+              <button className="group rounded-sm p-1 transition-colors hover:bg-zinc-800">
+                <Share className="h-6 w-6 fill-zinc-600 transition-colors group-hover:fill-green-500" />
+              </button>
+              <button
+                className="group rounded-sm p-1 transition-colors hover:bg-zinc-800"
+                onClick={() => setUser(null)}
+              >
+                <X className="h-6 w-6 fill-zinc-600 transition-colors group-hover:fill-red-500" />
+              </button>
+            </div>
+            <div className="flex w-full flex-col items-center justify-center">
+              <img
+                src={user.avatar_url}
+                alt={user.login}
+                className="h-36 w-36 rounded-full ring-2 ring-blue-700"
+              />
+              <div className="flex w-full flex-col items-center gap-2 p-4">
+                <div className="flex w-full flex-col items-center justify-center">
+                  <a
+                    href={`https://github.com/${user.login}`}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="rounded-sm bg-blue-700 bg-opacity-0 p-1 text-lg font-medium text-zinc-50 transition-colors hover:bg-opacity-20"
+                  >
+                    {user.login}
+                  </a>
+                  <p className="text-sm text-zinc-500">{user.bio}</p>
+                </div>
+                <div className="flex w-full justify-around">
+                  <span className="flex items-center gap-2 text-zinc-50">
+                    <Users className="h-5 w-5" />
+                    {user.followers}
+                  </span>
+                  <span className="flex items-center gap-2 text-zinc-50">
+                    <Archive className="h-5 w-5" />
+                    {user.public_repos}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col items-center justify-center">
+              <GithubLogo className="h-24 w-24 fill-zinc-100" />
+            </div>
+            <form
+              onSubmit={getUserData}
+              className="flex w-80 flex-col gap-3 p-2"
+            >
+              <div>
+                <input
+                  type="text"
+                  placeholder="Busque por um usuario no Github"
+                  className={`w-full rounded-sm bg-zinc-900 p-2 text-zinc-200 outline-none ring-1  placeholder:text-sm placeholder:text-zinc-500 focus:ring-zinc-500 ${
+                    showError ? 'ring-red-500' : 'ring-transparent'
+                  }`}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                {showError && (
+                  <span className="flex items-center gap-1 py-1 text-xs text-red-500">
+                    <WarningCircle />
+                    Usuario não encontrado, tente novamente
+                  </span>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-zinc-800 p-2 text-center font-medium text-zinc-50 transition-colors hover:bg-zinc-700"
+              >
+                Buscar
+              </button>
+            </form>
+          </>
+        )}
       </div>
     </main>
   )
